5e6f61b56bc330150b3347ba0723713699163197
Vishal Nagda <vishalnagda1@gmail.com>
2016-09-18 23:09:08 +0530

    Update subject model & done with controller's spec.

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -5,5 +5,22 @@ class Subject < ActiveRecord::Base
   has_and_belongs_to_many :students
 
   validates_presence_of :name, :school_ids, :classroom_ids
-  # validates_numericality_of :school_ids, :classroom_ids
+  validate :validate_classroom_ids
+  validate :validate_school_ids
+
+  private
+
+  def validate_classroom_ids
+    if classroom_ids.any?{ |id| !id.is_a?(Integer) }
+      errors.add(:classroom_ids, 'is not a number')
+      return false
+    end
+  end
+
+  def validate_school_ids
+    if school_ids.any?{ |id| !id.is_a?(Integer) }
+      errors.add(:school_ids, 'is not a number')
+      return false
+    end
+  end
 end
