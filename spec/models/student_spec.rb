4b41c32a02855295e503f5cd601e6aa237c30c00
Vishal Nagda <vishalnagda1@gmail.com>
2016-09-19 02:43:55 +0530

    update student model spec

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Student, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
+  context 'validation' do
+    ["name", "father_name", "mother_name", "city", "state", "zipcode", "phone", "school_id", "classroom_id"].each do |field|
+      it "should be invalid if #{field} is not present" do
+        FactoryGirl.build(:student, "#{field}".to_sym => nil).should_not be_valid
+      end
+    end
+
+    it { should validate_numericality_of(:phone) }
+    it { should validate_numericality_of(:zipcode) }
+    it { should validate_numericality_of(:school_id) }
+    it { should validate_numericality_of(:classroom_id) }
+    it { should validate_length_of(:phone).is_at_least(10) }
+  end
 end
